bf0f98fdce8b7713d1e8dc08512a8466d3c6ba0b
flexyyyapk213 <alexkornev1406@gmail.com>
2026-03-29 17:21:13 +0300

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -4,7 +4,10 @@ from pyrogram.client import Client
 from loads import Description, MainDescription, FuncDescription
 
 __description__ = Description(
-    MainDescription("Плагин для гриндилки ресурсов у ботов.")
+    MainDescription("Плагин для гриндилки ресурсов у ботов."),
+    FuncDescription('irisfarma'),
+    FuncDescription('skpromo'),
+    FuncDescription('bfgpromo')
 )
 
 import plugins.GrindBots.main
